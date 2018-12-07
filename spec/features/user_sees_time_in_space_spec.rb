525019661e8e2a297b61b94b7992a005347d1be0
Leah Miller <lkmiller.87@gmail.com>
2018-12-07 11:03:38 -0700

    Final commit

--- spec/features/user_sees_time_in_space_spec.rb
+++ spec/features/user_sees_time_in_space_spec.rb
@@ -4,15 +4,12 @@ describe 'astronaut index page' do
   describe 'as a visitor' do
     it 'sees astronaut information' do
       astronaut_1 = Astronaut.create(name: "John", age: 45, job: "pilot")
-      astronaut_2 = Astronaut.create(name: "Jim", age: 42, job: "captain")
-      mission_1 = astronaut_1.missions.create(title: "Apollo 13", time_in_space: 32)
-      mission_2 = astronaut_1.missions.create(title: "Capricorn 4", time_in_space: 42)
-      mission_3 = astronaut_2.missions.create(title: "Gemini 7", time_in_space: 39)
+      astronaut_1.missions.create(title: "Apollo 13", time_in_space: 32)
+      astronaut_1.missions.create(title: "Capricorn 4", time_in_space: 42)
 
       visit astronauts_path
 
-      expect(page).to have_content(astronaut_1.time_in_space)
-      expect(page).to have_content(astronaut_1.age)
+      expect(page).to have_content("#{Astronaut.total_time_in_space}")
 
     end
   end
